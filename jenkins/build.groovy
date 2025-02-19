42f39488a9aad2cf8fd08d6100516d7507397be7
likai <643609316@qq.com>
2025-02-19 09:28:38 +0800

    fix issues

--- jenkins/build.groovy
+++ jenkins/build.groovy
@@ -7,18 +7,20 @@ node('built-in') {
         tag = env.BRANCH_NAME.replace("/", "-").substring(0, Math.min(env.BRANCH_NAME.length(), 56)) + "-" + "${scmVars.GIT_COMMIT.substring(0, 6)}"
     }
 
-    stage('Build') {
-        sh 'npm config fix'
-        sh 'npm ci'
-        //sh 'npm run build'
-    }
+    nodejs(configId: 'npm-config', nodeJSInstallationName: nodeVersion) {
+        stage('Build') {
+            sh 'npm config fix'
+            sh 'npm ci'
+            //sh 'npm run build'
+        }
 
-    stage('Lint') {
-        sh 'npm run lint'
-    }
+        stage('Lint') {
+            sh 'npm run lint'
+        }
 
-    stage('Test') {
-        sh 'npm run test'
+        stage('Test') {
+            sh 'npm run test'
+        }
     }
 
     npmSonarV2 nodeVersion: nodeVersion,
