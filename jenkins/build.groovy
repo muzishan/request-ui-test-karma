afcf6acf983dbc6c8c5b70383306e711c5304e0d
likai <643609316@qq.com>
2025-02-20 13:40:59 +0800

    fix issues

--- jenkins/build.groovy
+++ jenkins/build.groovy
@@ -19,7 +19,7 @@ node('built-in') {
         }
 
         stage('Test') {
-            docker.image('zenika/alpine-chrome:with-node').inside("""--entrypoint=''""") {
+            docker.image('zenika/alpine-chrome:100-with-node-16').inside("""--entrypoint=''""") {
                 sh 'npm run test'
             }
             
